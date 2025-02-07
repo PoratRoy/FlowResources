594b72d105d550d0be5ed2f774c8c2943e34a5be
royporat <roy.porat@bloxroute.com>
2025-02-07 15:31:17 +0200

    fix

--- src/components/empty/NoWebsites/NoWebsites.tsx
+++ src/components/empty/NoWebsites/NoWebsites.tsx
@@ -3,20 +3,17 @@
 import React from 'react';
 import './NoWebsites.css';
 import AddWebsiteBtn from '@/components/UI/btn/AddWebsiteBtn/AddWebsiteBtn';
-import { Popups } from '@/models/enum';
-import { usePopupContext } from '@/context/PopupContext';
 
 type NoWebsitesProps = {
   text: string;
 };
 
 const NoWebsites: React.FC<NoWebsitesProps> = ({ text }) => {
-  const { openPopup } = usePopupContext();
 
   return (
     <section className="no-websites">
       <div>{text}</div>
-      <AddWebsiteBtn onClick={() => openPopup(Popups.addWebsite)} />
+      <AddWebsiteBtn />
     </section>
   );
 };
